b62e66dec8c10c25d15e359519e1befa1cdaa18b
Rave BizzDev <ravebizzdev@Raves-MacBook-Air.local>
2020-05-13 09:25:32 -0400

    Adding a few lines

--- HumanInterfaceGuidelines.playground/Contents.swift
+++ HumanInterfaceGuidelines.playground/Contents.swift
@@ -1,1 +1,14 @@
+Design Themes
 
+Clarity: precise, legible, negative space to highlight content
+Deference: fluid motion, content fill entire screen, minimal distractions
+Depth: multiple visual layers, relistic motion, touch/discoverability, use transitions
+
+Design Principles
+
+Aesthetic Integrity:
+Consistency
+Direct Manipulation
+Feedback
+Metaphors
+User Control:
