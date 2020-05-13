dc0a4b4c1764c055e402755b71fad17a9022d4cf
Rave BizzDev <ravebizzdev@Raves-MacBook-Air.local>
2020-05-13 09:29:57 -0400

    Adding a few lines

--- HumanInterfaceGuidelines.playground/Contents.swift
+++ HumanInterfaceGuidelines.playground/Contents.swift
@@ -6,9 +6,9 @@ Depth: multiple visual layers, relistic motion, touch/discoverability, use trans
 
 Design Principles
 
-Aesthetic Integrity:
-Consistency
-Direct Manipulation
-Feedback
-Metaphors
-User Control:
+Aesthetic Integrity: appearence and behavior should align with function
+Consistency: use well-known elements such as icons, text style, terminology
+Direct Manipulation: immediate/visial resutls of direct manipulation
+Feedback: highlight, progress indicator, animations, sounds
+Metaphors: use familar devices such as toggle switch, sliders, flicking
+User Control: user have the ultimate control, not the app
